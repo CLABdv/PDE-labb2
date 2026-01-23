5ec928e66830724bd63c061cfa7b80a2aa40e8f9
HSg33k <128152446+HSg33k@users.noreply.github.com>
2026-01-23 14:29:40 +0100

    Update uppg2.m

--- uppg2.m
+++ uppg2.m
@@ -1,20 +1,20 @@
-eps = 0.01;
-n=10;
-rs = linspace(eps,1,n);
+eps = 0.5;
+n=1000;
+rs = linspace(eps,1,n+1);
 vals = FEM(n, eps);
 figure(1)
 plot(rs,vals);
 hold on;
-%plot(rs, 1/(2*log(eps))*log(rs.^2));
+plot(rs, 1/(2*log(eps))*log(rs.^2));
 hold off;
 
 function vals = FEM(n, eps) 
     h = (1-eps)/n;
-    ATilde = sparse(n,n);
+    ATilde = sparse(n-1,n-1);
     node = @(i) eps + i*h;
     xWrapper = @(x) x*(x>=eps)*(x<=1);
-    for i=1:n
-        for j=1:n
+    for i=1:n-1
+        for j=1:n-1
             if abs(i-j) == 1
                 tmp = @(x) -1/h^2*xWrapper(x);
                 point = (node(i)+node(j))/2;
@@ -24,14 +24,22 @@ function vals = FEM(n, eps)
                 tmp = @(x) 1/h^2*xWrapper(x);
                 point1 = (node(i)+node(i-1))/2;
                 point2 = (node(i)+node(i+1))/2;     
-                ATilde(i,j) = h*(tmp(point1)+tmp(point2));
+                ATilde(i,j) = h*(tmp(point1)+tmp(point2))
             end
         end
     end
     
-    LTilde = zeros(n,1);
-    tmp = @(x) -1/h^2*xWrapper(x);
-    LTilde(1) = -h*tmp((node(1)+node(0))/2);
-    vals = ATilde \ LTilde;
-end
+    LTilde = zeros(n-1,1);
 
+    for i = 1:n-1
+        tmp1 = @(x) 1/(1-eps)*1/h*xWrapper(x);
+        tmp2 = @(x) -1/(1-eps)*1/h*xWrapper(x);
+        LTilde(i) = h*(tmp1((node(i-1)+node(i))/2)+ tmp2((node(i)+node(i+1))/2))
+    end
+    %tmp = @(x) -1/h^2*xWrapper(x);
+    %LTilde(1) = -h*tmp((node(1)+node(0))/2);
+    vals = ATilde \ LTilde;
+    vals = [0; vals; 0];
+    vals = vals + linspace(1, 0, n+1)';
+
+end
